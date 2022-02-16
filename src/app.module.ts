3829be816691a3611c6fc7b884ceec1303aa7622
Daniel <danielvfo@gmail.com>
2022-02-15 23:11:42 -0300

    setup typeorm postgres database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import dbConfiguration from './config/db.config';
+
 import { PokemonModule } from './pokemon/pokemon.module';
 import { TypeModule } from './type/type.module';
 
 @Module({
-  imports: [PokemonModule, TypeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfiguration],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+    PokemonModule,
+    TypeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/db.config.ts
@@ -0,0 +1,24 @@
+import { registerAs } from '@nestjs/config';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+
+export default registerAs('database', () => {
+  return {
+    type: 'postgres',
+    logging: true,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT),
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    autoLoadEntities: true,
+    // synchronize: process.env.MODE === "dev",
+    entities: ['src/**/*.entity.ts'],
+    migrations: ['src/migrations/*{.ts,.js}'],
+    cli: {
+      migrationsDir: 'src/migrations',
+    },
+    migrationsTableName: 'database_migrations',
+  };
+});
