6eb9d7ca8c4e3f7c290ececc717d768ca90936ce
Daniel <danielvfo@gmail.com>
2022-02-21 23:37:40 -0300

    add route to get pokemons by type

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { Pokemon } from './pokemon.entity';
+import { PokemonRepository } from './pokemon.repository';
+
+type FindByTypeArgs = { type: string };
 
 @Controller('pokemon')
-export class PokemonController {}
+export class PokemonController {
+  constructor(private readonly pokemonRepository: PokemonRepository) {}
+
+  @Get(':type')
+  async getAllPokemonsByType(
+    @Param() params: FindByTypeArgs,
+  ): Promise<Pokemon[]> {
+    return this.pokemonRepository.getAllByType(params.type);
+  }
+}
--- src/pokemon/pokemon.repository.ts
+++ src/pokemon/pokemon.repository.ts
@@ -2,4 +2,11 @@ import { Repository, EntityRepository } from 'typeorm';
 import { Pokemon } from './pokemon.entity';
 
 @EntityRepository(Pokemon)
-export class PokemonRepository extends Repository<Pokemon> {}
+export class PokemonRepository extends Repository<Pokemon> {
+  async getAllByType(type: string): Promise<Pokemon[]> {
+    return this.createQueryBuilder('pokemon')
+      .leftJoinAndSelect('pokemon.types', 'type')
+      .where('type.description = :description', { description: type })
+      .getMany();
+  }
+}
